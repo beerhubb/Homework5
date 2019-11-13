32733902fa249053e89511dabbc6fce4cb66f2fa
Chaiyawat Maduea <51731963+beerhubb@users.noreply.github.com>
2019-11-13 14:08:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            led ledd = new led();
+            ledd.DisplayLEDOnScreen("LED");
+        }
+    }
+}
