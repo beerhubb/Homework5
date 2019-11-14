fada2d93c75e63d97832c488e9c71713b87f72e0
Chaiyawat Maduea <51731963+beerhubb@users.noreply.github.com>
2019-11-15 03:42:09 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace NO1
                     for (int i = 1; i <=10; i++)
                     {
                         Console.Write($" {i} ");
+                        jkjkh
                     }
                     Console.WriteLine();
                 }
