046a8300bc320c292aa7850ba452ae36f3d11c64
Chaiyawat Maduea <51731963+beerhubb@users.noreply.github.com>
2019-11-15 02:59:32 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
-﻿namespace NO1
+using System;
+
+namespace NO1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            led ledd = new led();
-            ledd.DisplayLEDOnScreen("LED");
+            var ledd = new led();
+            try
+            {
+                var a = 1;
+                while (a < 2)
+                {
+                    var input = Console.ReadLine();
+                    var test = ledd.DisplayLEDOnScreen(input);
+                    Console.WriteLine(test);
+                    for (int i = 1; i <=10; i++)
+                    {
+                        Console.Write($" {i} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Error..");
+            }
         }
     }
 }
